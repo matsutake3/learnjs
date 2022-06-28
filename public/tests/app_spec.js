5d4960d212a489fbffb674bfcc02f622b51d208a
Akamatsu Takeshi <matsutake3@gmail.com>
2022-06-28 11:23:52 +0900

    2022/06/28 p47

---
+++ public/tests/app_spec.js
@@ -0,0 +1,26 @@
+describe('LearnJS', function(){
+   it('can show a problem view', function(){
+      learnjs.showView('#problem-1');
+      expect($('.view-container .problem-view').length).toEqual(1);
+   });
+   it('shows the landing page view when there is no hash', function(){
+      learnjs.showView('');
+      expect($('.view-container .landing-view').length).toEqual(1);
+   });
+   it('passes the hash view parameter to the view function', function() {
+      spyOn(learnjs, 'problemView');
+      learnjs.showView('#problem-42');
+      expect(learnjs.problemView).toHaveBeenCalledWith('42');
+   });
+   it('invoke the router when loaded', function(){
+      spyOn(learnjs, 'showView');
+      learnjs.appOnReady();
+      expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+   })
+   it('subscribe to the hash change event', function(){
+      learnjs.appOnReady();
+      spyOn(learnjs, 'showView');
+      $(window).trigger('hashchange');
+      expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+   })
+});
