c7d9496138d298612821ae37faa2d2cb50aab44e
Akamatsu Takeshi <matsutake3@gmail.com>
2022-09-03 11:53:21 +0900

    20220903

--- public/tests/SpecHelper.js
+++ public/tests/SpecHelper.js
@@ -3,7 +3,7 @@ var fixture;
 function loadFixture(path) {  
   var html;
   jQuery.ajax({
-    url: '/index.html',
+    url: '../index.html',
     success: function(result) {
       html = result;
     },
@@ -14,7 +14,7 @@ function loadFixture(path) {
 
 function resetFixture() {
   if (!fixture) {
-    var index = $('<div>').append(loadFixture('/index.html'));
+    var index = $('<div>').append(loadFixture('../index.html'));
     var markup = index.find('div.markup');
     fixture = $('<div class="fixture" style="display: none">').append(markup);
     $('body').append(fixture.clone());
